07123008b20a8c4fb3838c203b8b50ba05c8d2b6
aravindm7 <aravindaravind14636@gmail.com>
2023-10-30 16:56:43 +0000

    update

---
+++ js/main.js
@@ -0,0 +1,426 @@
+let foodcard = [
+{
+        id: 1,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/f115e9672d86fd9b874c2453168368e3",
+        shopName: "Indian Dhaba",
+        stateName: "South Indian, Sweets, North Indian, Chinese",
+        rating: 5.5,
+        timing: "30 ",
+        rate: "200 ",
+		qview:"View"
+    },
+    {
+        id: 2,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/o5vlkrbbjez8gbei4utj",
+        shopName: "A1 Fast Food",
+        stateName: "Indian, Chinese, Tandoor",
+        rating: "3.9",
+        timing: "21 ",
+        rate: " 250 ",
+			qview:" View"
+    },
+    {
+        id: 3,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/vciivwzggkt7tmemz6a6",
+        shopName: "Ambur Biriyani",
+        stateName: "Biriyani, Chinese",
+        rating: "4.0",
+        timing: "27 ",
+        rate: "100 ",
+			qview:" View"
+    },
+    {
+        id: 4,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/igxyfqbu82muvgsgusca",
+        shopName: "Munniyandi Villas",
+        stateName: "South Indian, Chinese",
+        rating: "4.2",
+        timing: "24 ",
+        rate: "300 ",
+			qview:" View"
+    },
+    {
+        id: 5,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/oe1jlbihfupegtcqfbdj",
+        shopName: "Kannappa Hotel",
+        stateName: "Biriyani, Chettinadu, Tandoor, Chinese",
+        rating: "3.5",
+        timing: "23 ",
+        rate: "400",
+			qview:" View"
+    },
+    {
+        id: 6,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/c9c2a0f3e5885d96c8260ff2f34cb190",
+        shopName: "Banana Leaf",
+        stateName: "Barbecue, tandoor, Indian, Kerala, Countinental, Juices, Desserts",
+        rating: "4.6",
+        timing: "25 ",
+        rate: "600 ",
+			qview:" View"
+    },
+    {
+        id: 7,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/igxyfqbu82muvgsgusca",
+        shopName: "Hotel Hakkim Biriyani",
+        stateName: "North Indian, Chiese, Briyani, Ice Cream, Desserts, Indian, Sounth Indian",
+        rating: "4.4",
+        timing: "26 ",
+        rate: "500 ",
+			qview:" View"
+    },
+    {
+        id: 8,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uw0obnnnva9ac1h3yvrf",
+        shopName: "Sudha's Kitchen",
+        stateName: "Biriyani ,Non veg, Veg,Indian,chinese",
+        rating: "4.4",
+        timing: "15 ",
+        rate: "300 ",
+			qview:" View"
+    },
+    {
+        id: 9,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/dcikspshyejprpaj6v5s",
+        shopName: "Hema's Hotel",
+        stateName: "North Indian, Chiese, Sounth Indian",
+        rating: "4.3",
+        timing: "30 ",
+        rate: "300 ",
+			qview:"View"
+    },
+    {
+        id: 10,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        shopName: "Bruger King",
+        stateName: "Burgers, Anerican",
+        rating: "4.1",
+        timing: "4.8 ",
+        rate: "350 ",
+			qview:" View"
+    },
+    {
+        id: 12,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hike11jtr2cwszrj2jz6",
+        shopName: "Bread Basket",
+        stateName: "Snacks , Dessarts , Pizza , Burger , cakes",
+        rating: 4.5,
+        timing: 19 ,
+        rate: 200 ,
+			qview:" View"
+    },
+    {
+        id: 13,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jcv2oxgfzixwjurtwor5",
+        shopName: "Hotel Mani",
+        stateName: "Biriyani,Tandoor, Chinese",
+        rating: "3.8",
+        timing: "33 ",
+        rate: "250 ",
+			qview:" View"
+    },
+    {
+        id: 14,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/chb45giivaibj7yfmntb",
+        shopName: "amman fast food",
+        stateName: "North Indian, South Indian, Biriyani ",
+        rating: "4.2",
+        timing: "36 ",
+        rate: "300 ",
+			qview:" View"
+    },
+    {
+        id: 15,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ulerfhz2lpvcu4jvn1dj",
+        shopName: "Black Forest",
+        stateName: "Bakery , cakes , Icecream",
+        rating: "4.2",
+        timing: "40 ",
+        rate: "150 ",
+			qview:" View"
+    },
+    {
+        id: 16,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/3ec8bccd0fc2c6303d7602dd642248d5",
+        shopName: "BG Naidu",
+        stateName: "Sweets , Snacks",
+        rating: "4.2",
+        timing: "24 ",
+        rate: "450 ",
+			qview:"View"
+    },
+    {
+        id: 17,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ezfln2uue5cu3tlxh9ve",
+        shopName: "Anu's Punjabi Dhaba",
+        stateName: "Chinese, North Indian",
+        rating: "4.4",
+        timing: "28 ",
+        rate: "800 ",
+			qview:" View"
+    },
+    {
+        id: 18,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/umgckxitnnyj2m3omhp2",
+        shopName: "Crick o Crick Kitchen",
+        stateName: "Indian, Chinese",
+        rating: "4.2",
+        timing: "40 ",
+        rate: "500 ",
+			qview:" View"
+    },
+    {
+        id: 19,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ksxgqcui31ccyzgcfgnh",
+        shopName: "Adayar AnandhaBhavan",
+        stateName: "Healthy Food, South Indian",
+        rating: "4.6",
+        timing: "21 ",
+        rate: "250 ",
+			qview:" View"
+    },
+    {
+        id: 20,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sxsgyafmg99w0cecedip",
+        shopName: "Thindukal Thalabakatti Biriyani",
+        stateName: "South Indian, Indian, Biriyani",
+        rating: "4.2",
+        timing: "30 ",
+        rate: "250",	
+		qview:"View"
+    },
+    {
+        id: 21,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/u3cqtyhaazgkmnej6ytv",
+        shopName: "Michell Icecreams",
+        stateName: "Icecream, Milkshakes",
+        rating: "4.2",
+        timing: "32",
+        rate: "400",
+			qview:" View"
+    },
+    {
+        id: 22,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/iwmuhpnmbzm2q4sfrpop",
+        shopName: "Ashwin Sweets",
+        stateName: "North Indian , Indian",
+        rating: "4.0",
+        timing: "38 ",
+        rate: "300",
+			qview:" View"
+    },
+    {
+        id: 23,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ee5f8e06b300efc07c9fe3f4df40dfc4",
+        shopName: "McDonald's",
+        stateName: "Burgers, Beverges, Cafe, Desserts",
+        rating: "4.3",
+        timing: "21",
+        rate: "400",
+			qview:"View"
+    },
+    {
+        id: 24,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ufpjwaow1wkb9gcqv01e",
+        shopName: "Udipi Hotel",
+        stateName: "North Indian, South Indain",
+        rating: "4.2",
+        timing: "34 ",
+        rate: "250",
+			qview:" View"
+    },
+    {
+        id: 25,
+        imageURL: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cmwqxtqo6zjkdfjxcjr0",
+        shopName: "Cake Bee",
+        stateName: "Bakery, Sweets, Desserts",
+        rating: "4.1",
+        timing: "23",
+        rate: "250",
+		qview:" View"
+    }
+]
+foodlist(foodcard)
+ 
+function foodlist(inputObj){ 
+ inputObj.forEach((e) => {
+ 	  let row=document.getElementById("row");
+
+ let col=document.createElement("div");
+ col.setAttribute("class","col-3");
+ 
+  let hold=document.createElement("div");
+ hold.setAttribute("class","hold");
+ 
+ 
+      let img=document.createElement("img");
+ img.setAttribute("src",e.imageURL);
+ 
+ let div=document.createElement("div")
+div.setAttribute("class","div");
+
+ 
+ let h3=document.createElement("h3");
+ h3.setAttribute("class","h3 fs-5");
+h3.innerHTML=e.shopName;
+
+ let para=document.createElement("p");
+ para.setAttribute("class","para");
+ para.innerHTML=e.stateName;
+
+let div_a=document.createElement("div");
+div_a.setAttribute("class","div_a");
+
+ 
+ let sp=document.createElement("span");
+ sp.setAttribute("class","span");
+ sp.innerHTML=e.rating;
+ 
+  let sp1=document.createElement("span");
+ sp1.setAttribute("class","span");
+ sp1.innerHTML=e.timing;
+ 
+  let sp2=document.createElement("span");
+ sp2.setAttribute("class","span");
+ sp2.innerHTML=e.rate;
+ 
+ let quick=document.createElement("div");
+ quick.setAttribute("class","quick");
+ let ach=document.createElement("a");
+ ach.setAttribute("class","ach");
+ ach.innerHTML=e.qview;
+ 
+ row.append(col);
+ col.appendChild(hold);
+hold.appendChild(img);
+ hold.appendChild(div);
+ div.appendChild(h3);
+ div.appendChild(para);
+ div.appendChild(div_a);
+ div_a.appendChild(sp);
+ div_a.appendChild(sp1);
+ div_a.appendChild(sp2);
+ div.appendChild(quick);
+ quick.append(ach);
+ 
+ col.addEventListener("click",function(){
+	
+	location.href="list.html"
+})
+ });
+}
+
+function shotrating(){
+	
+let first= foodcard.slice().sort(function (a,b){
+return b.rating-a.rating;
+});
+
+let removecol = document.querySelectorAll(".col-3");
+removecol.forEach((e)=>{
+	e.remove();
+	
+
+});
+foodlist(first);
+}
+
+function defaultShot() {
+
+    let removecol = document.querySelectorAll(".col-3");
+    removecol.forEach((e) => {
+        e.remove();
+    });
+
+    foodlist(foodcard); 
+}
+
+
+
+function shotdeliervy(){
+	
+let second= foodcard.slice().sort(function (a,b){
+return a.timing-b.timing;
+});
+
+let removecol = document.querySelectorAll(".col-3");
+removecol.forEach((e)=>{
+	e.remove();
+	
+
+});
+foodlist(second);
+}
+// cost low to high
+
+function costlow(){
+	
+let third= foodcard.slice().sort(function (a,b){
+return a.rate-b.rate;
+});
+
+let removecol = document.querySelectorAll(".col-3");
+removecol.forEach((e)=>{
+	e.remove();
+	
+
+});
+foodlist(third);
+}
+ // cost high to low
+
+ function costhigh(){
+	
+ let four= foodcard.slice().sort(function (a,b){
+ return b.rate-a.rate;
+ });
+
+let removecol = document.querySelectorAll(".col-3");
+removecol.forEach((e)=>{
+	 e.remove();
+	
+
+});
+foodlist(four);
+}
+const card = document.querySelectorAll(".card");
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("click", function () {
+
+        // console.log("btn wrked clicked");
+
+       
+    });
+
+}
+
+
+// swiggy login page
+
+ 
+let swiggyLogin = document.getElementById("swiggyLogin");
+
+swiggyLogin.addEventListener("click", function () {
+    location.href = "signin.html"
+})
+
+
+
+
+
+var owl = $('.brand');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
